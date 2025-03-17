4aafdcc12d3a347e53ca9ed758c918d1df4524ec
Kirvel48 <kirvel48@gmail.com>
2025-03-17 16:45:45 +0300

    addedAllureSteps

--- src/test/java/tests/PracticeFormWithFakerTests.java
+++ src/test/java/tests/PracticeFormWithFakerTests.java
@@ -9,6 +9,8 @@ import pages.components.TableResponseComponent;
 import utils.FakePersonMethods;
 import utils.StudentDataFactory;
 
+import static io.qameta.allure.Allure.step;
+
 @Tag("all")
 public class PracticeFormWithFakerTests extends TestBase {
     PracticeFormPage practiceFormPage = new PracticeFormPage();
@@ -21,73 +23,104 @@ public class PracticeFormWithFakerTests extends TestBase {
     @Test
     void fillPracticeFormTest() {
         StudentData student = studentDataFactory.generateStudentModelFull();
-        practiceFormPage.openPage()
-                .removeBanners()
-                .setFirstName(student.getFirstName())
-                .setLastName(student.getLastName())
-                .setUserEmail(student.getEmail())
-                .setGender(student.getGender())
-                .setPhoneNumber(student.getPhoneNumber())
-                .setSubjects(student.getSubject())
-                .setHobbies(student.getHobbies())
-                .uploadFile(student.getFileName())
-                .setCurrentAddress(student.getAddress())
-                .setState(student.getState())
-                .setCity(student.getCity());
-        calendarComponent.setDate(student.getDay(), student.getMonth(), student.getYear());
-        practiceFormPage.clickSubmit();
+        step("Open form", () -> {
+            practiceFormPage.openPage();
+        });
+        step("Remove banners", () -> {
+            practiceFormPage.removeBanners();
+        });
+        step("Fill form", () -> {
+            practiceFormPage.setFirstName(student.getFirstName())
+                    .setLastName(student.getLastName())
+                    .setUserEmail(student.getEmail())
+                    .setGender(student.getGender())
+                    .setPhoneNumber(student.getPhoneNumber())
+                    .setSubjects(student.getSubject())
+                    .setHobbies(student.getHobbies())
+                    .uploadFile(student.getFileName())
+                    .setCurrentAddress(student.getAddress())
+                    .setState(student.getState())
+                    .setCity(student.getCity());
 
-        tableResponseComponent.checkResult(student.getFirstName())
-                .checkResult(student.getLastName())
-                .checkResult(student.getEmail())
-                .checkResult(student.getGender())
-                .checkResult(student.getPhoneNumber())
-                .checkResult(student.getDay())
-                .checkResult(student.getMonth())
-                .checkResult(student.getYear())
-                .checkResult(student.getSubject())
-                .checkResult(student.getHobbies())
-                .checkResult(student.getFileName())
-                .checkResult(student.getAddress())
-                .checkResult(student.getState())
-                .checkResult(student.getCity());
+            calendarComponent.setDate(student.getDay(), student.getMonth(), student.getYear());
+        });
+        step("Click submit", () -> {
+            practiceFormPage.clickSubmit();
+        });
 
+        step("Check result", () -> {
+            tableResponseComponent.checkResult(student.getFirstName())
+                    .checkResult(student.getLastName())
+                    .checkResult(student.getEmail())
+                    .checkResult(student.getGender())
+                    .checkResult(student.getPhoneNumber())
+                    .checkResult(student.getDay())
+                    .checkResult(student.getMonth())
+                    .checkResult(student.getYear())
+                    .checkResult(student.getSubject())
+                    .checkResult(student.getHobbies())
+                    .checkResult(student.getFileName())
+                    .checkResult(student.getAddress())
+                    .checkResult(student.getState())
+                    .checkResult(student.getCity());
+
+        });
     }
 
     @Test
     @Tag("smoke")
     void fillPracticeMandatoryFormTest() {
         StudentData studentMandatoru = studentDataFactory.generateStudentModelMandatory();
-        practiceFormPage.openPage();
-        practiceFormPage.removeBanners()
-                .setFirstName(studentMandatoru.getFirstName())
-                .setLastName(studentMandatoru.getLastName())
-                .setGender(studentMandatoru.getGender())
-                .setPhoneNumber(studentMandatoru.getPhoneNumber());
-        calendarComponent.setDate(studentMandatoru.getDay(), studentMandatoru.getMonth(), studentMandatoru.getYear());
-
-        practiceFormPage.clickSubmit();
-        tableResponseComponent.checkResult(studentMandatoru.getFirstName())
-                .checkResult(studentMandatoru.getLastName())
-                .checkResult(studentMandatoru.getGender())
-                .checkResult(studentMandatoru.getPhoneNumber())
-                .checkResult(studentMandatoru.getDay())
-                .checkResult(studentMandatoru.getMonth())
-                .checkResult(studentMandatoru.getYear());
+        step("Open form", () -> {
+            practiceFormPage.openPage();
+        });
+        step("Remove banners", () -> {
+            practiceFormPage.removeBanners();
+        });
+        step("Fill form", () -> {
+            practiceFormPage.setFirstName(studentMandatoru.getFirstName())
+                    .setLastName(studentMandatoru.getLastName())
+                    .setGender(studentMandatoru.getGender())
+                    .setPhoneNumber(studentMandatoru.getPhoneNumber());
+            calendarComponent.setDate(studentMandatoru.getDay(), studentMandatoru.getMonth(), studentMandatoru.getYear());
+        });
+        step("Click submit", () -> {
+            practiceFormPage.clickSubmit();
+        });
+        step("Check result", () -> {
+            tableResponseComponent.checkResult(studentMandatoru.getFirstName())
+                    .checkResult(studentMandatoru.getLastName())
+                    .checkResult(studentMandatoru.getGender())
+                    .checkResult(studentMandatoru.getPhoneNumber())
+                    .checkResult(studentMandatoru.getDay())
+                    .checkResult(studentMandatoru.getMonth())
+                    .checkResult(studentMandatoru.getYear());
+        });
     }
 
     @Test
     @Tag("smoke")
     void fillPracticeFormNegativeTest() {
-        practiceFormPage.openPage()
-                .removeBanners()
-                .setFirstName(fakePerson.getFirstName())
-                .clickSubmit()
-                .modalWithResultNotExist();
-
+        step("Open form", () -> {
+            practiceFormPage.openPage();
+        });
+        step("Remove banners", () -> {
+            practiceFormPage.removeBanners();
+        });
+        step("Fill form", () -> {
+            practiceFormPage.setFirstName(fakePerson.getFirstName());
+        });
+        step("Click submit", () -> {
+            practiceFormPage.clickSubmit();
+        });
+        step("Check result", () -> {
+            practiceFormPage.modalWithResultNotExist();
+        });
     }
 
 }
+
+
 
 
 
